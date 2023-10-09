31e55e9dc43a740b971e502490fff00557aa40db
jtgomba <jgomba@hotmail.co.uk>
2023-10-09 14:15:14 +0900

    feat: added search and filters

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./Searchbar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, NavBar, Footer };
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
