22ad3420203a66cce9e0a30d09f87e25d56e058d
jtgomba <jgomba@hotmail.co.uk>
2023-10-09 13:51:59 +0900

    feat: added navbar and footer

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 import CustomButton from "./CustomButton";
 import Hero from "./Hero";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, NavBar, Footer };
